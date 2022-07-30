5e92c60a850b0517c3186fb3204c67944d25ef8a
Shindou Mihou <kokocrunchsh@gmail.com>
2022-07-30 20:29:44 +0800

    Trim content before deserializing with the extensions.

--- src/main/kotlin/pw/mihou/snail/snql/SNQLExtensions.kt
+++ src/main/kotlin/pw/mihou/snail/snql/SNQLExtensions.kt
@@ -1,12 +1,12 @@
 package pw.mihou.snail.snql
 
 fun String.toSNQLObject(): Map<String, Any?>? {
-    val objectMatcher = SNQLObject.REGEX.matchEntire(this) ?: return null
+    val objectMatcher = SNQLObject.REGEX.matchEntire(this.trim()) ?: return null
     return SNQLObject.parse(objectMatcher.groups["object"]!!.value)
 }
 
 fun String.toSNQLArray(): List<Any?>? {
-    val arrayMatcher = SNQLArray.REGEX.matchEntire(this) ?: return null
+    val arrayMatcher = SNQLArray.REGEX.matchEntire(this.trim()) ?: return null
     return SNQLArray.parse(arrayMatcher.groups["array"]!!.value)
 }
 
